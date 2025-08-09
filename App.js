ffcff3b61552825e7a1208ed2f835a1ff2026976
chendyRewa <chendy.rewa@outlook.com>
2025-08-09 17:35:39 -0400

    react program

---
+++ App.js
@@ -0,0 +1,5 @@
+const headReact=React.createElement('h1',{id:'rootReact'},'Reya Chandel');
+const reactRoot=ReactDOM.createRoot(document.getElementById('rootReact'));
+ reactRoot.render(headReact);
+ console.log(headReact);
+ console.log(reactRoot);
